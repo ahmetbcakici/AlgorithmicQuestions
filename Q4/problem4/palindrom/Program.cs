0cebd13ca482e2db6fb6d08d4cfd52e4adbeff5e
ahmetbcakici <ahmetbcakici@gmail.com>
2019-06-25 18:27:37 +0300

    Q4 added

---
+++ Q4/problem4/palindrom/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool palindromecontrol = false;
+            Console.Write("Enter a word:");
+            string word = Console.ReadLine();
+            for (int i = 0, j = word.Length - 1; i < word.Length; i++,j--) 
+            {
+                if (word[i] != word[j])
+                {
+                    palindromecontrol = true;
+                    break;
+                }
+            }
+            if(palindromecontrol == false)
+                Console.WriteLine(word+" is a palindrome word");
+            Console.ReadKey();
+        }
+    }
+}
