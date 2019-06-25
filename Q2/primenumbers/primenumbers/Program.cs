6b5c06689f0a1e3685b7af98038ebbd2fe908eac
ahmetbcakici <ahmetbcakici@gmail.com>
2019-06-25 15:40:54 +0300

    Initial Commit

---
+++ Q2/primenumbers/primenumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primenumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int cntr = 0;
+            bool c = false;
+            for (int i = 2; ; i++)
+            {
+                if (cntr >= number) break;
+                else
+                {
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            c = true;
+                            break;
+                        }
+                    }
+                    if (c == false)
+                    {
+                        Console.Write(i + " ");
+                        cntr++;
+                    }
+                    c = false;
+                }
+            }
+            Console.Read();
+        }
+    }
+}
