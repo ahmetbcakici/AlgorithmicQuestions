be18323078129c53834a0eb60adaaf3392eaf28f
ahmetbcakici <ahmetbcakici@gmail.com>
2019-06-26 07:30:31 +0300

    Added answers in different languages

---
+++ Q1/Answer in C#/answer/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    sum += i;
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
