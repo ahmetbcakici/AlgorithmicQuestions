ec64048c57ba3a4ed80c95b76a78ed5b7bcfcdae
ahmetbcakici <ahmetbcakici@gmail.com>
2019-06-28 07:04:17 +0300

    Q8's answer updated

--- Q8/Answer in Python/answer.py
+++ Q8/Answer in Python/answer.py
@@ -3,10 +3,8 @@ max = 0
 for i in range(1000,99,-1):
     for j in range(1000,99,-1):
         result = i * j
-        for k, l in zip(range(0, len(str(result))), range(len(str(result)) - 1, -1, -1)):
-            if str(result)[k] != str(result)[l]:
-                flag = True
-                break
+        if str(result)[::-1] != str(result):
+            flag = True
         if flag != True:
             if(result > max):
                 max = result
